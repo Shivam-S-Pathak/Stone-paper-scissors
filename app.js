b5b521d1dafdf9938b47038cc93f8fe1ebe09778
Shivam Pathak <shivampathak5042.com>
2024-06-06 14:34:39 +0530

    bug fix adding number

--- app.js
+++ app.js
@@ -1,71 +1,63 @@
-const choices=document.querySelectorAll(".choice");
-const msg= document.querySelector(".message");
-const userWinning=document.querySelector(".userscore");
-const compWinning=document.querySelector(".compscore");
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector(".message");
+const userWinning = document.querySelector(".userscore");
+const compWinning = document.querySelector(".compscore");
 const resetBtn = document.querySelector(".reset-btn");
 
-let userScore=0;
-let compScore=0;
-let userWin =true;
+let userScore = 0;
+let compScore = 0;
+let userWin = true;
 
-let gencompChoice= ()=>{
-    const option=["stone","paper","scissors"];
-    const randIdx = Math.floor(Math.random()* option.length);
-    return option[randIdx];
+let gencompChoice = () => {
+  const option = ["stone", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * option.length);
+  return option[randIdx];
+};
 
-}
+const playGame = (userChoice) => {
+  const compChoice = gencompChoice();
+  if (userChoice === compChoice) {
+    msg.innerText = "Its a draw";
+    msg.style.backgroundColor = "black";
+  } else {
+    if (userChoice === "stone") {
+      userWin = compChoice === "scissors" ? true : false;
+    } else if (userChoice === "paper") {
+      userWin = compChoice === "stone" ? true : false;
+    } else if (userChoice === "scissors") {
+      userWin = compChoice === "paper" ? true : false;
+    }
 
-const playGame= (userChoice)=>{
-    const compChoice=gencompChoice();
-if (userChoice===compChoice){
-msg.innerText="Its a draw";
-msg.style.backgroundColor="black";
-}else{
-    if (userChoice==="stone"){
-        userWin = (compChoice==="scissors"?true:false);
-   }
-   else if(userChoice==="paper"){
-    userWin=(compChoice==="stone"?true:false);
-   }
-   else if(userChoice==="scissors"){
-    userWin=(compChoice==="paper"?true:false);
-   }
-    
-    checkWinner(userChoice , compChoice , userWin);
+    checkWinner(userChoice, compChoice, userWin);
+  }
+};
 
-}
-
-}
-
-const checkWinner = (userChoice, compChoice)=>{
-   if (userWin){
+const checkWinner = (userChoice, compChoice) => {
+  if (userWin) {
     userScore++;
-    userWinning.innerText=userScore;
-    msg.innerText="You win 😃, " + userChoice + " beats " + compChoice;
-    msg.style.backgroundColor="green";
-
-   }
-   else{
+    userWinning.innerText = userScore;
+    msg.innerText = "You win 😃, " + userChoice + " beats " + compChoice;
+    msg.style.backgroundColor = "green";
+  } else {
     compScore++;
-    compWinning.innerText=compScore;
-    msg.innerText="You lost 😟, " + compChoice + " beats " + userChoice;
-    msg.style.backgroundColor="red";
-   }
-}
+    compWinning.innerText = compScore;
+    msg.innerText = "You lost 😟, " + compChoice + " beats " + userChoice;
+    msg.style.backgroundColor = "red";
+  }
+};
 
-resetBtn.addEventListener('click', ()=>{
-    userWinning.innerText="0";
-    compWinning.innerText="0";
-    msg.style.backgroundColor="black";
-    msg.innerText="Play your move";
+resetBtn.addEventListener("click", () => {
+  userScore = 0;
+  compScore = 0;
+  userWinning.innerText = "0";
+  compWinning.innerText = "0";
+  msg.style.backgroundColor = "black";
+  msg.innerText = "Play your move";
+});
 
-})
-
-choices.forEach((choice)=>{
-    choice.addEventListener('click' , ()=>{
-        let userChoice=choice.getAttribute("id");
-        playGame(userChoice);
-
-
-    })
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    let userChoice = choice.getAttribute("id");
+    playGame(userChoice);
+  });
 });
